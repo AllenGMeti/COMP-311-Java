f596f789db002eb789dadda37d11627a3fd4376f
AllenGMeti <metiallen027@gmail.com>
2026-08-29 18:16:37 +0200

    Add Question 1, personal profile.

---
+++ Labs/Question_1.java
@@ -0,0 +1,23 @@
+/*
+ *Allen G Meti
+ *24019726
+ *Lab 1 Question 1 
+ */
+
+public class Question_1 {
+  public static void main(String[] args){
+
+//Declaring and Assigning values to variables
+  String name = "Allen G Meti";
+  int age = 20;
+  double gpa = 5.00;
+  boolean fulltime = true;
+
+//Printing variables 
+  System.out.println("Name: " + name);
+  System.out.println("Age: " + age);
+  System.out.println("GPA: " + gpa);
+  System.out.println("fulltime: " + fulltime);
+
+  }
+}
