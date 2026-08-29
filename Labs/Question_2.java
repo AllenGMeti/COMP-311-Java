e6f5720cb97f544be224776e50b7825b58f63dde
AllenGMeti <metiallen027@gmail.com>
2026-08-29 20:37:41 +0200

    Add Question 2, primitive playground

---
+++ Labs/Question_2.java
@@ -0,0 +1,31 @@
+/*
+ *Allen G Meti
+ *24019726
+ *Lab 1 Question 2
+ */
+
+public class Question_2{
+  public static void main( String[] args){
+
+//Declaring and Assigning values to variables
+  byte b = 100;
+  short s = 20000;
+  int i = 350000;
+  long l = 9000000000L;
+  float f = 3.14f;
+  double d = 3.14159265;
+  char c = 'J';
+  boolean bool = true;
+
+//Printing variables
+  System.out.println("byte: " + b);
+  System.out.println("short: " + s);
+  System.out.println("int: " + i);
+  System.out.println("long: " + l);
+  System.out.println("float: " + f);
+  System.out.println("double: " + d);
+  System.out.println("char: " + c);
+  System.out.println("boolean: " + bool);
+
+  }
+}
