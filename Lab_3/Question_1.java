a505f554f8875d8b5f99999c7bb8f41f571b1af0
AllenGMeti <metiallen027@gmail.com>
2026-09-13 13:55:21 +0200

    Adding Question 1

---
+++ Lab_3/Question_1.java
@@ -0,0 +1,21 @@
+/*
+ * Allen G Meti
+ * 24019726
+ * Lab 3 Question 1
+ */
+
+public class Question_1 {
+    public static void main(String[] args) {
+
+        int count = 10;
+
+        // Loop for count down from 10 to 1
+        while (count >= 1) {
+            System.out.println(count);
+            count--; 
+        }
+
+        // Printing liftoff message
+        System.out.println("Liftoff!");
+    }
+}
