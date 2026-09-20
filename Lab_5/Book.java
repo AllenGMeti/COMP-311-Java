dd7b1d6f1cc744270bd46143312326c501d2fd5d
AllenGMeti <metiallen027@gmail.com>
2026-09-20 23:17:38 +0200

    Adding Question 1

---
+++ Lab_5/Book.java
@@ -0,0 +1,46 @@
+/*
+ * Allen G Meti
+ * 24019726
+ * Lab 5 Question 1,2,4,5
+ */
+
+public class Book {
+
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getSummary() {
+        return title + " " + author + " " + price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
