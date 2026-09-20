83f399d91a554ef21d3bc567ca2a2987a44ea3fb
AllenGMeti <metiallen027@gmail.com>
2026-09-20 23:22:48 +0200

    Add Question 3

---
+++ Lab_5/Question_3.java
@@ -0,0 +1,16 @@
+/*
+ * Allen G Meti
+ * 24019726
+ * Lab 5 Question 3
+ */
+
+public class Question_3 {
+   public static void main(String[] args) {
+
+   Book book1 = new Book("Java Labs", "Allen Meti", 120.50);
+   Book book2 = new Book("Java", "Goabaone Meti", 150.75);
+
+   System.out.println(book1.getSummary());
+   System.out.println(book2.getSummary());
+  }
+}
